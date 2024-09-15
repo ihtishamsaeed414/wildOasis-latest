b92060ed9bb899693d9a43384e9260f0c89b941d
ihtisham saeed <ihtishamsaeed7780@gmail.com>
2024-09-16 00:19:13 +0500

    react query completed

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -5,15 +5,6 @@ import { useState } from "react";
 import CreateCabinForm from "./CreateCabinForm";
 import { useDeleteCabin } from "./useDeleteCabin";
 import { useCreatteCabin } from "./useCreateCabin";
-// import Menus from "ui/Menus";
-// import Modal from "ui/Modal";
-// import ConfirmDelete from "ui/ConfirmDelete";
-// import Table from "ui/Table";
-
-// import { useDeleteCabin } from "./useDeleteCabin";
-// import { useCreateCabin } from "./useCreateCabin";
-// import CreateCabinForm from "./CreateCabinForm";
-
 // v1
 const TableRow = styled.div`
   display: grid;
@@ -109,77 +100,3 @@ function CabinRow({ cabin }) {
 }
 
 export default CabinRow;
-// const {
-//   id: cabinId,
-//   name,
-//   maxCapacity,
-//   regularPrice,
-//   discount,
-//   image,
-//   description,
-// } = cabin;
-// const { mutate: deleteCabin, isLoading: isDeleting } = useDeleteCabin();
-// const { mutate: createCabin } = useCreateCabin();
-// function handleDuplicate() {
-//   createCabin({
-//     name: `${name} duplicate`,
-//     maxCapacity,
-//     regularPrice,
-//     discount,
-//     image,
-//     description,
-//   });
-// }
-// return (
-//   <Table.Row role='row'>
-//     <Img src={image} alt={`Cabin ${name}`} />
-//     <Cabin>{name}</Cabin>
-//     <div>Fits up to {maxCapacity} guests</div>
-//     <Price>{formatCurrency(regularPrice)}</Price>
-//     {discount ? (
-//       <Discount>{formatCurrency(discount)}</Discount>
-//     ) : (
-//       <span>&mdash;</span>
-//     )}
-//     <Modal>
-//       <Menus.Menu>
-//         <Menus.Toggle id={cabinId} />
-//         <Menus.List id={cabinId}>
-//           <Menus.Button icon={<HiSquare2Stack />} onClick={handleDuplicate}>
-//             Duplicate
-//           </Menus.Button>
-//           <Modal.Toggle opens='edit'>
-//             <Menus.Button icon={<HiPencil />}>Edit cabin</Menus.Button>
-//           </Modal.Toggle>
-//           {/* Now it gets a bit confusing... */}
-//           <Modal.Toggle opens='delete'>
-//             <Menus.Button icon={<HiTrash />}>Delete cabin</Menus.Button>
-//           </Modal.Toggle>
-//         </Menus.List>
-//       </Menus.Menu>
-//       {/* This needs to be OUTSIDE of the menu, which in no problem. The compound component gives us this flexibility */}
-//       <Modal.Window name='edit'>
-//         <CreateCabinForm cabinToEdit={cabin} />
-//       </Modal.Window>
-//       <Modal.Window name='delete'>
-//         <ConfirmDelete
-//           resource='cabin'
-//           onConfirm={() => deleteCabin(cabinId)}
-//           disabled={isDeleting}
-//         />
-//       </Modal.Window>
-//     </Modal>
-//     {/* <div>
-//       <ButtonWithConfirm
-//         title='Delete cabin'
-//         description='Are you sure you want to delete this cabin? This action can NOT be undone.'
-//         confirmBtnLabel='Delete'
-//         onConfirm={() => deleteCabin(cabinId)}
-//         disabled={isDeleting}
-//       >
-//         Delete
-//       </ButtonWithConfirm>
-//       <Link to={`/cabins/${cabinId}`}>Details &rarr;</Link>
-//     </div> */}
-//   </Table.Row>
-// );
--- src/features/settings/UpdateSettingsForm.jsx
+++ src/features/settings/UpdateSettingsForm.jsx
@@ -1,4 +1,4 @@
-import Form from ", ../.. /ui/Form";
+import Form from "../../ui/Form";
 import FormRow from "../../ui/FormRow";
 import Input from "../../ui/Input";
 import Spinner from "../../ui/Spinner";
@@ -20,8 +20,10 @@ function UpdateSettingsForm() {
 
   if (isLoading) return <Spinner />;
 
-  function handleUpdate(e) {
+  function handleUpdate(e, field) {
     const { value } = e.target;
+    if (!value) return;
+    updateSetting({ [field]: value });
   }
   return (
     <Form>
@@ -30,12 +32,19 @@ function UpdateSettingsForm() {
           type="number"
           id="min-nights"
           defaultValue={minBookingLength}
+          disabled={isUpdating}
           onBlur={(e) => handleUpdate(e, "minBookingLength")}
         />
       </FormRow>
 
       <FormRow label="Maximum nights/booking">
-        <Input type="number" id="max-nights" defaultValue={maxBookingLength} />
+        <Input
+          type="number"
+          id="max-nights"
+          defaultValue={maxBookingLength}
+          disabled={isUpdating}
+          onBlur={(e) => handleUpdate(e, "maxBookingLength")}
+        />
       </FormRow>
 
       <FormRow label="Maximum guests/booking">
@@ -43,6 +52,8 @@ function UpdateSettingsForm() {
           type="number"
           id="max-guests"
           defaultValue={maxGuestsPerBooking}
+          disabled={isUpdating}
+          onBlur={(e) => handleUpdate(e, "maxGuestsPerBooking")}
         />
       </FormRow>
 
@@ -51,6 +62,8 @@ function UpdateSettingsForm() {
           type="number"
           id="breakfast-price"
           defaultValue={breakfastPrice}
+          disabled={isUpdating}
+          onBlur={(e) => handleUpdate(e, "breakfastPrice")}
         />
       </FormRow>
     </Form>
