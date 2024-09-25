eb10f8347a521178a09e7a10b1741270b47a6960
ihtisham saeed <ihtishamsaeed7780@gmail.com>
2024-09-25 12:26:40 +0500

    Modal implementation using compund component

--- src/features/cabins/AddCabin.jsx
+++ src/features/cabins/AddCabin.jsx
@@ -1,11 +1,24 @@
-import { useState } from "react";
 import Modal from "../../ui/Modal";
 import Button from "../../ui/Button";
 import CreateCabinForm from "./CreateCabinForm";
+import CabinTable from "./CabinTable";
 function AddCabin() {
-  const [isOpenModal, setIsOpenModal] = useState(false);
-
   return (
+    <Modal>
+      <Modal.Open opens="cabin-form">
+        <Button>Add new Cabin</Button>
+      </Modal.Open>
+      <Modal.Window name="cabin-form">
+        <CreateCabinForm />
+      </Modal.Window>
+
+      <Modal.Open opens="table">
+        <Button>Add new Cabin</Button>
+      </Modal.Open>
+      <Modal.Window name="table">
+        <CabinTable />
+      </Modal.Window>
+    </Modal>
     // <Modal>
     //   <Modal.Toggle opens='new-cabin'>
     //     <Button>Add new cabin</Button>
@@ -14,16 +27,16 @@ function AddCabin() {
     //     <CreateCabinForm />
     //   </Modal.Window>
     // </Modal>
-    <>
-      <Button onClick={() => setIsOpenModal((show) => !show)}>
-        Add new cabin
-      </Button>
-      {isOpenModal && (
-        <Modal onClose={() => setIsOpenModal(false)}>
-          <CreateCabinForm onCloseModal={() => setIsOpenModal(false)} />
-        </Modal>
-      )}
-    </>
+    // <>
+    //   <Button onClick={() => setIsOpenModal((show) => !show)}>
+    //     Add new cabin
+    //   </Button>
+    //   {isOpenModal && (
+    //     <Modal onClose={() => setIsOpenModal(false)}>
+    //       <CreateCabinForm onCloseModal={() => setIsOpenModal(false)} />
+    //     </Modal>
+    //   )}
+    // </>
   );
 }
 
